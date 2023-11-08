74c3a9d80eafdb57f1b14ee157847958ffd4789d
alperenikinci <alperen.ikinci@hotmail.com>
2023-11-08 11:41:08 +0300

    Optionlar icin saveAll metodu entegre edildi...
    Signed-off-by: alperenikinci <alperen.ikinci@hotmail.com>

---
+++ src/main/java/com/bilgeadam/controller/OptionController.java
@@ -0,0 +1,41 @@
+package com.bilgeadam.controller;
+
+import com.bilgeadam.dto.request.OptionSaveRequestDto;
+import com.bilgeadam.dto.response.OptionResponseDto;
+import com.bilgeadam.repository.QuestionRepository;
+import com.bilgeadam.repository.entity.Option;
+import com.bilgeadam.repository.entity.Question;
+import com.bilgeadam.service.OptionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.bilgeadam.constants.RestApi.*;
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(OPTION)
+public class OptionController {
+
+    private final OptionService optionService;
+
+    private final QuestionRepository questionRepository;
+
+
+    @PostMapping(SAVE)
+    public ResponseEntity<OptionResponseDto> saveOption(@RequestBody OptionSaveRequestDto dto){
+        return ResponseEntity.ok(optionService.saveOption(dto));
+    }
+
+
+    @PostMapping(SAVEALL)
+    public ResponseEntity<List<OptionResponseDto>> saveAllOptions(@RequestBody List<OptionSaveRequestDto> dtoList){
+        Question question = new Question();
+        question = questionRepository.save(question);
+        return ResponseEntity.ok(optionService.saveAllOptions(dtoList,question));
+    }
+}
